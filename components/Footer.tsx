53fc26046df397c7a30e9669ad2aad5b740fb3fc
rahmadikaaa <rahmadika.putera@gmail.com>
2026-02-05 13:07:31 +0700

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,13 +6,13 @@ export const Footer: React.FC = () => {
     <footer className="bg-cyber-darker border-t border-slate-900 py-12">
       <div className="container mx-auto px-6 text-center">
         <div className="flex justify-center gap-8 mb-8">
-          <a href="#" className="text-slate-500 hover:text-cyber-primary transition-colors transform hover:scale-110">
+          <a href="https://github.com/rahmadikaaa/rahmadika" className="text-slate-500 hover:text-cyber-primary transition-colors transform hover:scale-110">
             <Github size={24} />
           </a>
-          <a href="#" className="text-slate-500 hover:text-cyber-secondary transition-colors transform hover:scale-110">
+          <a href="https://www.linkedin.com/in/rahmadikaa/" className="text-slate-500 hover:text-cyber-secondary transition-colors transform hover:scale-110">
             <Linkedin size={24} />
           </a>
-          <a href="mailto:contact@rahmadika.dev" className="text-slate-500 hover:text-white transition-colors transform hover:scale-110">
+          <a href="mailto:rahmadika.putera@gmail.com" className="text-slate-500 hover:text-white transition-colors transform hover:scale-110">
             <Mail size={24} />
           </a>
         </div>
